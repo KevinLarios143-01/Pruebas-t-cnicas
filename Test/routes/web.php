1ed4dd74f3eac813deb5d5582e04d6e05ec1c253
KLARIOS <alexander.larios@galileo.edu>
2023-05-12 02:19:27 -0600

    KLARIOS - Se agregan acciones para el crud

--- Test/routes/web.php
+++ Test/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/clientes', [App\Http\Controllers\ClienteController::class, 'index'])->name('clientes');
+Route::get('/nuevosclientes', function(){return view('nuevocliente');});
+Route::post('NuevosC', [App\Http\Controllers\ClienteController::class, 'registro']);
+Route::get('EliminarC/{id}', [App\Http\Controllers\ClienteController::class, 'eliminar']);
+
+
+Route::get('/productos', [App\Http\Controllers\ProductoController::class, 'index'])->name('productos');
+Route::get('/nuevosproductos', function(){return view('nuevoproducto');});
+Route::post('NuevosP', [App\Http\Controllers\ProductoController::class, 'registro']);
+Route::get('EliminarP/{id}', [App\Http\Controllers\ProductoController::class, 'eliminar']);
+
+
+Route::get('/facturas', [App\Http\Controllers\FacturaController::class, 'index'])->name('facturas');
+Route::get('/nuevasfacts', function(){return view('crearfacturas');});
+Route::post('NuevasF', [App\Http\Controllers\FacturaController::class, 'registro']);
+Route::get('EliminarF/{id}', [App\Http\Controllers\FacturaController::class, 'eliminar']);
+
