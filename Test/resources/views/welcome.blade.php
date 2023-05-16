dc94ee5011a895a8e28593d60d81c3e61bb65866
KLARIOS <alexander.larios@galileo.edu>
2023-05-16 09:17:51 -0600

    KLARIOS - Instrucciones

--- Test/resources/views/welcome.blade.php
+++ Test/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Inicio</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,115 +14,75 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <!-- Styles -->
-
 </head>
 
 <body class="antialiased">
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                @auth
-                    <a href="{{ url('/home') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                @else
-                    <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-        <style>
-            /* Carousel styling */
-            #introCarousel,
-            .carousel-inner,
-            .carousel-item,
-            .carousel-item.active {
-                height: 100vh;
-            }
-
-            .carousel-item:nth-child(1) {
-                background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg');
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center center;
-            }
-
-            .carousel-item:nth-child(2) {
-                background-image: url('https://mdbootstrap.com/img/Photos/Others/images/77.jpg');
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center center;
-            }
-
-            .carousel-item:nth-child(3) {
-                background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg');
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center center;
-            }
-
-            /* Height for devices larger than 576px */
-            @media (min-width: 992px) {
-                #introCarousel {
-                    margin-top: -58.59px;
-                }
-
-                #introCarousel,
-                .carousel-inner,
-                .carousel-item,
-                .carousel-item.active {
-                    height: 50vh;
-                }
-            }
-
-            .navbar .nav-link {
-                color: #fff !important;
-            }
-        </style>
-        <!-- Carousel wrapper -->
-        </header>
-        <!--Main Navigation-->
-
-        <!--Main layout-->
-        <main class="mt-5">
-            <div class="container">
-                <!--Section: Content-->
-                <section>
-                    <div class="row">
-                        <div class="col-md-6 gx-5 mb-4">
-                            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5"
-                                data-mdb-ripple-color="light">
-                                <img src="https://mdbootstrap.com/img/new/slides/031.jpg" class="img-fluid" />
-                                <a href="#!">
-                                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 gx-5 mb-4">
-                            <h4><strong>PRUEBA DE LARAVEL PHP.</strong></h4>
-                            <p class="text-muted">
-                                Se instalaron algunos cosas para poder hacer funcionar el proyecto, como composer, nodejs,
-                                algunas extensiones de sqlServer, y se instalo el paquete de laravel/ui para poder hacer
-                                funcionar el login y el register. Se realizaron los migrates con el ORM de laravel.
-
-                                Me disculpo por no haberlo terminado, pero no me dio tiempo ya que no había utilizado el ORM
-                                directamente.
-                            </p>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            @if (Route::has('login'))
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://i.ibb.co/TkM65BZ/logo.png" alt="Logo" width="30" height="24"
+                            class="d-inline-block align-text-top">
+                        Bootstrap
+                    </a>
+                    @auth
+                        <a class="navbar-brand" href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a class="navbar-brand" href="{{ route('login') }}">Log in</a>
+                        @if (Route::has('register'))
+                            <a class="navbar-brand" href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </nav>
+    </header>
+    <!--Main layout-->
+    <main class="mt-5">
+        <div class="container">
+            <!--Section: Content-->
+            <section>
+                <div class="row">
+                    <div class="col-md-6 gx-5 mb-4">
+                        <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5"
+                            data-mdb-ripple-color="light">
+                            <img src="https://mdbootstrap.com/img/new/slides/031.jpg" class="img-fluid" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </a>
                         </div>
                     </div>
-                </section>
-                <!--Section: Content-->
 
-                <hr class="my-5" />
+                    <div class="col-md-6 gx-5 mb-4">
+                        <h4><strong>PRUEBA DE LARAVEL PHP.</strong></h4>
+                        <p class="text-muted" style="text-align: justify">
+                            Se instalaron algunos cosas para poder hacer funcionar el proyecto, como composer, nodejs,
+                            algunas extensiones de sqlServer, y se instalo el paquete de laravel/ui para poder hacer
+                            funcionar el login y el register. Se realizaron los migrates con el ORM de laravel.
 
-            </div>
-    </body>
+                            <ul>
+                                <li>
+                                    Se utilizó el Eloquent ORM, se generaron los migrates para la creación de las tablas y su estructura.
+                                    La tabla principal se llama pruebas.
+                                </li>
+                                <li>
+                                    Se debe ejecutar el comando php artisan migrate para crear las tablas y poder generar un nuevo usuario
+                                    para poder ingresar al sistema.
+                                </li>
+
+                            </ul>
+
+                            Me disculpo por no haberlo terminado, pero no me dio tiempo ya que no había utilizado el ORM
+                            directamente.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <!--Section: Content-->
+            <hr class="my-5" />
+        </div>
+    </main>
+</body>
+
 </html>
